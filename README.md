5c4512550e2db0a7d2e687352539f1f921a76920
Dan Moore <bitbucket@mooreds.com>
2020-04-21 16:34:41 -0600

    Added ability to get a previous created user.

--- README.md
+++ README.md
@@ -3,3 +3,15 @@ An example of using the FusionAuth API with .NET core.
 
 This application uses FusionAuth APIs to add a user and then search for users from the command line. This can be used to pull a subset of data from the FusionAuth database, for instance to get the assigned laptop number.
 
+## To run
+
+```
+cd usermanager
+fusionauth_api_key=<yourkey here> dotnet.exe run
+```
+
+Output:
+```
+retrieved user with email: dotnetcore@example.com
+```
+
--- usermanager/Program.cs
+++ usermanager/Program.cs
@@ -1,12 +1,27 @@
 ﻿using System;
+using io.fusionauth;
 
 namespace usermanager
 {
     class Program
     {
+        private static readonly string apiKey = Environment.GetEnvironmentVariable("fusionauth_api_key");
+        private static readonly string fusionauthURL = "http://localhost:9011";
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            FusionAuthSyncClient client = new FusionAuthSyncClient(apiKey, fusionauthURL);
+            var response = client.RetrieveUserByEmail("dotnetcore@example.com");
+            if (response.WasSuccessful())
+            {
+                var user = response.successResponse.user;
+                Console.WriteLine("retrieved user with email: "+user.email);
+            } 
+            else if (response.statusCode != 200) 
+            {
+                var statusCode = response.statusCode;
+                Console.WriteLine("failed with status "+statusCode);
+            } 
         }
     }
 }
